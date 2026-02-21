29c432d85e1f23a2e59413092772c42d6d771afb
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-21 13:27:57 +0300

    [Feature] Added waterfall layout for collection

--- Unsplash Gallery/FeedCollection/FeedCollectionExtension.swift
+++ Unsplash Gallery/FeedCollection/FeedCollectionExtension.swift
@@ -7,9 +7,9 @@
 
 import UIKit
 
-extension FeedCollection: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+extension FeedCollection: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        2 // make computed property
+        20 // make computed property
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -17,14 +17,5 @@ extension FeedCollection: UICollectionViewDelegateFlowLayout, UICollectionViewDa
             return UICollectionViewCell()
         }
         return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let spacing: CGFloat = 5
-        let totalWidth = collectionView.bounds.width
-        let itemsPerRow: CGFloat = 2
-        let totalSpacing = spacing * itemsPerRow
-        let itemWidth = (totalWidth - totalSpacing) / itemsPerRow
-        return CGSize(width: itemWidth, height: itemWidth / 1.25)
     }
 }
