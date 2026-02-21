d4534dea78dc05cd8f8133b1d27567d705f98be8
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-21 11:45:22 +0300

    [Feature] Added basic file structure

---
+++ Unsplash Gallery/UI/MainScreen.swift
@@ -0,0 +1,8 @@
+//
+//  MainScreen.swift
+//  Unsplash Gallery
+//
+//  Created by Игнат Рогачевич on 21.02.26.
+//
+
+import Foundation
---
+++ Unsplash Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash Gallery
+//
+//  Created by Игнат Рогачевич on 21.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
