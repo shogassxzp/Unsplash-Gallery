20671556927e53dcb59b3bb6dc7b4d24c8584b92
Ignat  Rogachevich <rssxzp@icloud.com>
2026-02-28 17:57:04 +0300

    [Refactor] updated ProfileService for property injection

--- Unsplash Gallery/Networking/Services/ProfileService.swift
+++ Unsplash Gallery/Networking/Services/ProfileService.swift
@@ -9,37 +9,50 @@ import Foundation
 
 final class ProfileService {
     static let shared = ProfileService()
-    private init() {}
-
+    
+    // MARK: - Dependencies
+    var urlSession = URLSession.shared
+    var tokenStorage = OAuth2TokenStorage.shared
+    
+    // MARK: - Private Properties
     private(set) var username: String?
     private var task: URLSessionTask?
+    
+    private init() {}
 
+    // MARK: - Public Methods
     func fetchProfile(completion: @escaping (Result<String, Error>) -> Void) {
         assert(Thread.isMainThread)
         if task != nil { return }
 
-        guard let request = makeRequest() else { return }
+        guard let request = makeRequest() else {
+            completion(.failure(NetworkError.invalidRequest))
+            return
+        }
 
-        let task = URLSession.shared.objectTask(for: request) { [weak self] (result: Result<ProfileResult, Error>) in
+        let task = self.urlSession.objectTask(for: request) { [weak self] (result: Result<ProfileResult, Error>) in
             DispatchQueue.main.async {
+                guard let self = self else { return }
                 switch result {
                 case let .success(profile):
-                    self?.username = profile.username
+                    self.username = profile.username
                     completion(.success(profile.username))
                 case let .failure(error):
                     completion(.failure(error))
                 }
-                self?.task = nil
+                self.task = nil
             }
         }
         self.task = task
         task.resume()
     }
 
+    // MARK: - Private Methods
     private func makeRequest() -> URLRequest? {
         guard let url = URL(string: "https://api.unsplash.com/me") else { return nil }
         var request = URLRequest(url: url)
-        guard let token = OAuth2TokenStorage.shared.token else { return nil }
+        
+        guard let token = tokenStorage.token else { return nil }
         request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         return request
     }
