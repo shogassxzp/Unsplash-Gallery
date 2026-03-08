05b18c4851d6fad4ddf1925f1c9da6aa7f7a4f76
Ignat  Rogachevich <rssxzp@icloud.com>
2026-03-09 01:20:23 +0300

    [feature] returned empty state

--- Unsplash Gallery/Presentation/FavouriteScreen/FavoriteScreen.swift
+++ Unsplash Gallery/Presentation/FavouriteScreen/FavoriteScreen.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 final class FavouriteViewController: UIViewController {
+    
+    // MARK: - UI Elements
     private let collection = FeedCollection()
     private let viewModel: FavoritesViewModel
     private let imageListService: ImageListService
 
+    private lazy var emptyStateStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 12
+        stack.alignment = .center
+        stack.isHidden = true
+
+        let imageView = UIImageView(image: UIImage(resource: .favEmpty))
+        imageView.contentMode = .scaleAspectFit
+
+        let label = UILabel()
+        label.text = "You haven't liked any photos yet"
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .blackAdaptive
+
+        stack.addArrangedSubview(imageView)
+        stack.addArrangedSubview(label)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+
+    // MARK: - Init
     init(viewModel: FavoritesViewModel, imageListService: ImageListService) {
         self.viewModel = viewModel
         self.imageListService = imageListService
@@ -22,14 +46,19 @@ final class FavouriteViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupNavigationBarTitle(text: "Favourite", imageName: "heart")
+        
         collection.viewModel = self.viewModel
         collection.isReadOnlyMode = true
+        
+        setupBindings()
+        
         imageListService.fetchLikedPhotosNextPage()
-
+        
         collection.onPhotoTap = { [weak self] index in
             guard let self = self else { return }
             let detailsViewModel = DetailsViewModel(
@@ -40,45 +69,48 @@ final class FavouriteViewController: UIViewController {
             let detailsViewController = DetailsScreenViewController(viewModel: detailsViewModel)
             self.navigationController?.pushViewController(detailsViewController, animated: true)
         }
-        viewModel.onDataUpdated = { [weak self] in
-            guard let self = self else { return }
-            let count = self.viewModel.photosCount
-            
-            DispatchQueue.main.async {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
-                    self.collection.reloadData()
-                    self.collection.collectionViewLayout.invalidateLayout()
-                    self.collection.layoutIfNeeded()
-                }
-            }
-        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        collection.viewModel = self.viewModel
-        self.collection.reloadData()
-        self.collection.collectionViewLayout.invalidateLayout()
-        self.collection.layoutIfNeeded()
-        
-        DispatchQueue.main.async {
-            self.collection.collectionViewLayout.invalidateLayout()
-            self.collection.layoutIfNeeded()
+        updateUIState()
+    }
+
+    // MARK: - Private Methods
+    private func setupBindings() {
+        viewModel.onDataUpdated = { [weak self] in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                self.updateUIState()
+            }
         }
     }
 
+    private func updateUIState() {
+            let isEmpty = viewModel.photosCount == 0
+            emptyStateStack.isHidden = !isEmpty
+            collection.isHidden = isEmpty
+        }
+
     private func setupUI() {
         view.backgroundColor = .backgroundAdaptive
-        collection.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(collection)
+        view.addSubview(emptyStateStack)
+        
+        collection.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             collection.topAnchor.constraint(equalTo: view.topAnchor),
             collection.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collection.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            collection.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            collection.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            emptyStateStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyStateStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emptyStateStack.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+            emptyStateStack.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
         ])
     }
 }
-
